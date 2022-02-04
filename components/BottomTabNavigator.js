4874502a6cf8a68c0fc686f39f7e69613661e2f5
WHJR_PRO-1:4 <72431927+rashmi-sathya@users.noreply.github.com>
2022-02-04 17:48:34 +0530

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -47,16 +47,7 @@ export default class BottomTabNavigator extends Component {
               fontSize: 20,
             },
             labelPosition: "beside-icon",
-            tabStyle: {
-              marginTop: 25,
-              marginLeft: 10,
-              marginRight: 10,
-              borderRadius: 30,
-              borderWidth: 2,
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "#5653d4"
-            }
+           
           }}
         >
           <Tab.Screen name="Transaction" component={TransactionScreen} />
